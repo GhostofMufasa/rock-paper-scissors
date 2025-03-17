5090ab1cb1ffddb94ba8034c66b30c378bfa16e3
GhostofMufasa <123848045+GhostofMufasa@users.noreply.github.com>
2025-03-17 14:23:54 -0700

    Function to prompt user choice

--- game.js
+++ game.js
@@ -3,3 +3,8 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function getHumanChoice() {
+    const userInput = prompt("Enter your choice (rock, paper, or scissors): ");
+    return userInput.toLowerCase();
+}
